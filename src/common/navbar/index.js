58f02ad921c60959eb56c684342dc8cdce1aac84
Raymond Lee <raylee0525@gmail.com>
2017-11-21 16:28:50 -0800

    Implement navbar

--- src/common/navbar/index.js
+++ src/common/navbar/index.js
@@ -4,7 +4,19 @@ class Navbar extends React.Component {
   render() {
     return (
       <div className="navbar-container">
-        Hi
+        <div className="navbar-left">
+          <img 
+            className="logo"
+            src="http://res.cloudinary.com/rlee0525/image/upload/v1511309375/logo_gpnitx.png"
+          />
+          <h1 className="app-name">RxNorm</h1>
+        </div>
+
+        <div className="navbar-right">
+          <a href='https://github.com/rlee0525/RxNorm' target="_blank">
+            <i className="fa fa-github" aria-hidden="true"></i>
+          </a>
+        </div>
       </div>
     );
   }
